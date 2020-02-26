6e3a5a2c44c21701031a5a1453da32ecd9adb073
Антон <tonic1995@gmail.com>
2020-02-27 01:47:01 +0300

    Добален класс команд, основные кнопки, выбор файлов

---
+++ AutoVisaConcept/AutoVisaConcept/VM/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoVisaConcept.VM
+{
+    class ViewModel
+    {
+        private Relay_Command _download;
+
+
+        public List<string> FilePaths;
+
+        public bool OpenFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FilePaths = openFileDialog.FileNames.ToList();
+                return true;
+            }
+            return false;
+        }
+
+        public Relay_Command Download
+        {
+            get
+            {
+                return _download ??
+                  (_download = new Relay_Command(obj => OpenFileDialog()));
+            }
+        }
+    }
+}
