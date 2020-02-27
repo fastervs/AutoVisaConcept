afbdbeecdf3d46ecf9c7062c8f0cea66d8e11ee3
Антон <tonic1995@gmail.com>
2020-02-28 01:17:05 +0300

    Тест

---
+++ AutoVisaConcept/AutoVisaConcept/Model/Person.cs
@@ -0,0 +1,116 @@
+﻿using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AutoVisaConcept.Model
+{
+    class Person:DependencyObject
+    {
+
+        public string FilePath
+        {
+            get { return (string)GetValue(FilePathpr); }
+            set { SetValue(FilePathpr, value); }
+        }
+        public static readonly DependencyProperty FilePathpr =
+            DependencyProperty.Register("FilePath", typeof(string), typeof(Person), new PropertyMetadata(""));
+
+        public string Name {
+            get { return (string)GetValue(Namepr); }
+            set { SetValue(Namepr, value); }
+        }
+
+        public static readonly DependencyProperty Namepr =
+            DependencyProperty.Register("Name", typeof(string), typeof(Person), new PropertyMetadata(""));
+
+        public string Surname
+        {
+            get { return (string)GetValue(Surnamepr); }
+            set { SetValue(Surnamepr, value); }
+        }
+
+        public static readonly DependencyProperty Surnamepr =
+            DependencyProperty.Register("Surname", typeof(string), typeof(Person), new PropertyMetadata(""));
+        public string Patronomic
+        {
+            get { return (string)GetValue(Patronomicpr); }
+            set { SetValue(Patronomicpr, value); }
+        }
+
+        public static readonly DependencyProperty Patronomicpr =
+            DependencyProperty.Register("Patronomic", typeof(string), typeof(Person), new PropertyMetadata(""));
+
+
+        public string Birthdate
+        {
+            get { return (string)GetValue(Birthdatepr); }
+            set { SetValue(Birthdatepr, value); }
+        }
+
+        public static readonly DependencyProperty Birthdatepr =
+            DependencyProperty.Register("Birthdate", typeof(string), typeof(Person), new PropertyMetadata(""));
+
+
+        public int? Passport_id
+        {
+            get { return (int?)GetValue(Passport_idpr); }
+            set { SetValue(Passport_idpr, value); }
+        }
+
+        public static readonly DependencyProperty Passport_idpr =
+            DependencyProperty.Register("Passport_id", typeof(int?), typeof(Person), new PropertyMetadata(0));
+
+
+        AutoOcr Ocr = new AutoOcr();
+
+        
+        public Person(string FilePath1)
+        {
+            FilePath = FilePath1;
+            
+        }
+        
+        public async Task OcrProceed()
+        {
+            var Results = Ocr.Read(FilePath);
+            StringReader strReader = new StringReader(Results.Text);
+            string aLine = null;
+            while (true)
+            {
+                aLine = strReader.ReadLine();
+                if (aLine != null)
+                {
+                    if (aLine.Contains("<<<<"))
+                    {
+                        string[] temp1 = (aLine.Substring(5)).Split(new string[] { "<<" }, StringSplitOptions.None);
+                        Surname = temp1[0];
+                        Name = temp1[1].Split('<')[0];
+                        Patronomic = temp1[1].Split('<')[1];
+                    }
+                    else if (aLine.Length<25)//((aLine.Contains("МУЖ") || aLine.Contains("ЖЕН")))//&&(Regex.Matches(aLine, "\\d\\d.\\d\\d.\\d\\d\\d\\d").Count!=0))
+                    {
+                        try
+                        {
+                            Birthdate =Regex.Matches(aLine, "\\d\\d.\\d\\d.\\d\\d\\d\\d")[0].Value;
+                        }
+                        catch {
+                            try
+                            {
+                                Birthdate = Regex.Matches(aLine, "\\d\\d.\\s\\d\\d.\\d\\d\\d\\d")[0].Value.Trim();
+                            }
+                            catch { }
+                        }
+                        //Console.WriteLine(res);
+                    }
+                }
+                else { break; }
+            }
+        }
+    }
+}
--- AutoVisaConcept/AutoVisaConcept/VM/ViewModel.cs
+++ AutoVisaConcept/AutoVisaConcept/VM/ViewModel.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.Win32;
+﻿using AutoVisaConcept.Model;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using IronOcr;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Data;
 
 namespace AutoVisaConcept.VM
 {
@@ -11,26 +17,45 @@ namespace AutoVisaConcept.VM
     {
         private Relay_Command _download;
 
+        public ObservableCollection<Person> Persons { get; set; }
 
-        public List<string> FilePaths;
+        public Person temp;
 
-        public bool OpenFileDialog()
+        private readonly object _urlsLock = new object();
+
+        public ViewModel()
+        {
+            //
+            Persons = new ObservableCollection<Person>();
+            BindingOperations.EnableCollectionSynchronization(Persons, _urlsLock);
+        }
+
+        public void OpenFileDialog()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
             if (openFileDialog.ShowDialog() == true)
             {
-                FilePaths = openFileDialog.FileNames.ToList();
-                return true;
+                //await Task.Run(OcrProceed());
+                foreach (var strr in openFileDialog.FileNames)
+                {
+                    temp = new Person(strr);
+                    temp.OcrProceed();
+                    //Persons.Add(temp);
+                    //OcrProceed(File);
+                }
             }
-            return false;
         }
+
+        
+
 
         public Relay_Command Download
         {
             get
             {
                 return _download ??
-                  (_download = new Relay_Command(obj => OpenFileDialog()));
+                  (_download = new Relay_Command(obj =>  OpenFileDialog()));
             }
         }
     }
